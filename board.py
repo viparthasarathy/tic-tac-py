f010e7189346277c500a9d92de7d12dd6ecc2eff
viparthasarathy <viparthasarathy@gmail.com>
2016-09-28 15:12:03 -0700

    Add .is_over() and is_tie() to Board class

--- board.py
+++ board.py
@@ -1,5 +1,4 @@
 class Board(object):
-
     def __init__(self):
         self.tiles = []
         for _ in range(3):
@@ -10,3 +9,18 @@ class Board(object):
       for row in self.tiles:
           print(" %s | %s | %s " % (row[0], row[1], row[2]))
           print("-----------")
+
+    def is_winner(self):
+        
+    def is_over(self):
+        return self.is_winner or self.is_tie
+
+    def is_tie(self):
+        space = False
+        for row in self.tiles:
+            for tile in row:
+                if tile == " ":
+                    space = True
+        return space
+
+    def play(self):
---
+++ player.py
@@ -0,0 +1,7 @@
+class Player(object):
+
+    def __init__(self, board):
+        self.board = board
+
+    def make_move(tile):
+        
