ec9a9808cda28ad9d2118c1fc7c81bb8c168e895
viparthasarathy <viparthasarathy@gmail.com>
2016-09-28 14:27:39 -0700

    Add .display() to Board class

--- board.py
+++ board.py
@@ -1,8 +1,12 @@
 class Board(object):
 
     def __init__(self):
-        self.board = []
+        self.tiles = []
         for _ in range(3):
-            self.board.append(["", "", ""])
+            self.tiles.append([" ", " ", " "])
+        self.turn = 0
 
-    
+    def display(self):
+      for row in self.tiles:
+          print(" %s | %s | %s " % (row[0], row[1], row[2]))
+          print("-----------")
